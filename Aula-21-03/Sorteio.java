5130ec4de9cbe0e133cb32c6ba091908639c5b14
lucasoalima <lucas_soalima@hotmail.com>
2022-03-21 22:11:13 -0300

    Adicionado Media no Sorteio

--- Aula-21-03/Sorteio.java
+++ Aula-21-03/Sorteio.java
@@ -6,11 +6,16 @@ public class Sorteio {
         public static void main(String[] args) {
             Random gerador = new Random();
             int c = 1, num;
-
+            double media = 0; //numero de ponto flutuante
+            
             while (c <= 1000){
                 num = 1 + gerador.nextInt(100);
+                media += num; // media = media + numero
                 System.out.println(num);
                 c++;
             }
+            media /= 1000.0d; // media = media / 1000;
+            // media = media / (double) 1000;
+            System.out.println("Média = " + media);
         }
 }
