4c680853edce7fb66e2c3c34f7182633db8a5505
Lucas Lima <lucas_soalima@hotmail.com>
2022-03-23 16:56:42 -0300

    Ex 03 - Fatorial adicionado

--- EX_3/ExFatorial.java
+++ EX_3/ExFatorial.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class ExFatorial {
-    
+    public static void main(String[] args){
+        int numero;
+        int i = 1;
+
+        System.out.println("FAVOR INFORMAR O NUMERO QUE DESEJA CALCULAR O FATORIAL: ");
+        Scanner scanner = new Scanner(System.in);
+        numero = scanner.nextInt();
+        long fatorial = 1;
+
+        if(numero > 0){
+        while(i <= numero){
+            fatorial = fatorial * i;
+            i++;   
+        }
+        System.out.println("O FATORIAL DE: " + numero + " É " + fatorial);}
+        else{
+            System.out.println("ERRO: NUMERO INVALIDO");
+        }
+        
+    }
 }
